0dc4b0ad7a923e516f0b05a3869db6e374a005ab
super usuario vagrant <vagrant@vagrant.vagrant-debian-amd64>
2016-10-03 11:14:07 -0500

    agrega Funcionalidad Figuras Agrega archivo travis agrega archivo requerimientos

---
+++ Figuras.py
@@ -0,0 +1,30 @@
+class Figuras:
+
+    def cuadrado(self, lado):
+        try:
+            if lado % 1 == 0:
+                return lado * lado
+            else:
+                return float(lado * lado)
+        except Exception, e:
+            return 'dato incorrecto'
+
+    def rectangulo(self, base, altura):
+        try:
+            if base % 1 == 0 and altura % 1 == 0:
+                return base * altura
+            else:
+                return float(base * altura)
+        except Exception, e:
+            return 'dato incorrecto'
+
+    def triangulo(self, base, altura):
+        try:
+            if base % 1 == 0 and altura % 1 == 0:
+                return (base * altura) / 2
+            else:
+                res = float(base * altura) / 2
+
+                return format(res, .2f)
+        except Exception, e:
+            return 'dato incorrecto'
---
+++ FigurasTest.py
@@ -0,0 +1,55 @@
+import unittest
+from figuras import Figuras
+
+
+class TestFiguras(unittest.TestCase):
+
+    def setUp(self):
+        self.figuras = Figuras()
+
+    def test_area_cuadrado_lado_5(self):
+
+        resultado = self.figuras.cuadrado(5)
+        self.assertEqual(25, resultado)
+
+    def test_area_cuadrado_lado_6(self):
+
+        resultado = self.figuras.cuadrado(6)
+        self.assertEqual(36, resultado)
+
+    def test_area_cuadrado_lado_g(self):
+
+        resultado = self.figuras.cuadrado('g')
+        self.assertEqual('dato incorrecto', resultado)
+
+    def test_area_cuadrado_lado_4_punto_5(self):
+
+        resultado = self.figuras.cuadrado(4.5)
+        self.assertEqual(20.25, resultado)
+
+    def test_area_rectangulo_base_5_altura_6(self):
+
+        resultado = self.figuras.rectangulo(5, 6)
+        self.assertEqual(30, resultado)
+
+    def test_area_rectangulo_base_5_punto_5_altura_6_punto_4(self):
+
+        resultado = self.figuras.rectangulo(5.5, 6.4)
+        self.assertEqual(35.2, resultado)
+
+    def test_area_rectangulo_g_f(self):
+
+        resultado = self.figuras.rectangulo('g', 'f')
+        self.assertEqual('dato incorrecto', resultado)
+
+    def test_area_triangulo_base8_altura_7(self):
+
+        resultado = self.figuras.triangulo(8, 7)
+        self.assertEqual(28, resultado)
+
+    def test_area_triangulo_base8_punto_3_altura_7_punto_2(self):
+
+        resultado = self.figuras.triangulo(8.3, 7.2)
+        self.assertEqual(29.88, resultado)
+if __name__ == '__main__':
+    unittest.main()
